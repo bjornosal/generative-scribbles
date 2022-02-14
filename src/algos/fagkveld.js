56ac4a6f2d197747a51310dd198ba597673bf1ef
Bjørn Olav Salvesen <bjorn.olav.salvesen@gmail.com>
2022-02-14 18:57:54 +0100

    Fixed torus with new base

---
+++ src/algos/fagkveld.js
@@ -0,0 +1,123 @@
+import { getGlobalParameters } from "../parameters";
+
+const sketch = (p) => {
+    let { printSize, scaleRatio, exportRatio, palette, torusAmount, randomStartingPoint } = getGlobalParameters();
+    let buffer;
+    let canvas;
+    //TODO:
+    let printingSize = printSize ?? {
+        width: 3508,
+        height: 2480,
+    };
+
+    const startingPoints = generateRandomStartingPoints(
+        torusAmount,
+        p.width,
+        p.height,
+        p
+    );
+
+    p.setup = () => {
+        let w = printingSize.width / exportRatio;
+        let h = printingSize.height / exportRatio;
+        buffer = p.createGraphics(w, h, p.WEBGL);
+        canvas = p.createCanvas(w, h, p.WEBGL);
+        // Adjust according to screens pixel density.
+        exportRatio /= p.pixelDensity();
+        //Do your setup here ⬇️
+
+
+    };
+
+    p.draw = () => {
+        p.background("" + palette.background ?? "#FFF");
+        // Clear buffer each frame
+        buffer.clear();
+        // Transform (scale) all the drawings
+        buffer.scale(scaleRatio);
+        //Draw here :) ⬇️
+
+
+
+        p.pixelDensity(20)
+        p.stroke("purple");
+        p.strokeWeight(4);
+        p.noFill();
+        p.rect(-p.width / 2, -p.height / 2, p.width, p.height);
+        p.strokeWeight(1);
+
+        for (let i = 0; i < parameters.torusAmount; i++) {
+            if (randomStartingPoint) {
+                let startingPoint = startingPoints[i];
+                drawTorus(startingPoint.x, startingPoint.y);
+            } else {
+                drawTorus(0, 0);
+            }
+        }
+
+        //Stop drawing here ⬆️
+        // Draw buffer to canvas
+        p.image(buffer, 0, 0);   
+    };
+
+
+    const drawTorus = (x, y) => {
+        p.push();
+        p.translate(x, y);
+        p.rotateX(p.frameCount * 0.01);
+        p.rotateY(p.frameCount * 0.01);
+        const torusWidth = p.height / 8;
+        p.torus(torusWidth, torusWidth / 2);
+        p.pop();
+    };
+
+    const exportHighResolution = () => {
+        scaleRatio = exportRatio;
+        // Re-create buffer with exportRatio and re-draw
+        buffer = p.createGraphics(scaleRatio * p.width, scaleRatio * p.height);
+        p.draw();
+        // Get timestamp to name the ouput file
+        let timestamp = new Date().getTime();
+        // Save as PNG
+        p.save(buffer, p.str(timestamp), "png");
+        // Reset scaleRation back to original, re-create buffer, re-draw
+        scaleRatio = 1;
+        buffer = p.createGraphics(p.width, p.height);
+        p.draw();
+    };
+
+    p.keyReleased = () => {
+        if (p.key == "e" || p.key == "E") {
+            exportHighResolution();
+        }
+    };
+};
+
+const generateRandomStartingPoints = (amount, width, height, p) => {
+    const startingPoints = [];
+
+    for (let i = 0; i < amount; i++) {
+        let randomX = p.random(-width / 3, width / 3);
+        let randomY = p.random(-height / 3, height / 3);
+
+        let x = parseInt(randomX);
+        let y = parseInt(randomY);
+
+        startingPoints.push({ x, y });
+    }
+
+    return startingPoints;
+};
+
+const name = "Fagkveld2";
+
+const parameters = { torusAmount: 1, randomStartingPoint: false };
+
+const addFolder = (gui) => {
+    const folder = gui.addFolder(name);
+    folder.add(parameters, "torusAmount", 1, 10, 1);
+    folder.add(parameters, "randomStartingPoint");
+    return folder;
+};
+
+export { name, sketch, addFolder, parameters };
--- src/algos/index.js
+++ src/algos/index.js
@@ -1,5 +1,6 @@
 import * as fagkveld from "./fagkveldSketch";
+import * as fagkveld2 from "./fagkveld";
 import * as sineWave from "./sineWave";
 import * as base from "./base";
 
-export default { fagkveld, sineWave, base };
+export default { fagkveld, fagkveld2, sineWave, base };
