81bb1b30685d0e604b51b188ab0d454b0fe401c8
Bjørn Olav Salvesen <bjorn.olav.salvesen@gmail.com>
2022-02-27 22:16:40 +0100

    Add squares and triangles drawing

--- src/algos/index.js
+++ src/algos/index.js
@@ -2,10 +2,12 @@ import * as torus from "./torus";
 import * as sineWave from "./sineWave";
 import * as base from "./base"; 
 import * as particles from "./particles"; 
+import * as squares from "./squares"; 
 
 export default { 
   torus, 
-  sineWave, 
+  sineWave,
+  squares, 
   base,
   particles 
 };
