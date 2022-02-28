496022cd28f2e8a29a00cd9ff8d3c90b712c5f01
Bjørn Olav Salvesen <bjorn.olav.salvesen@gmail.com>
2022-02-28 19:18:30 +0100

    Add algo during flight

---
+++ src/algos/flight.js
@@ -0,0 +1,103 @@
+import { getGlobalParameters } from "../parameters";
+
+// These values needs to be changed.
+const name = "Flight";
+const parameters = { frameRate: 30 };
+const addFolder = (gui) => {
+    const folder = gui.addFolder(name);
+    folder.add(parameters, "frameRate", 1, 200, 5).name("Frame rate");
+    return folder;
+};
+
+const sketch = (p) => {
+    let {
+        printSize,
+        scaleRatio,
+        exportRatio,
+        palette,
+        frameRate,
+    } = getGlobalParameters();
+    let buffer;
+    let canvas;
+    let printingSize = printSize ?? {
+        width: 3508,
+        height: 2480,
+    };
+    //No guarantee these values are set.
+    //Background is a string with hex value
+    //Colors is an array of strings with hex value
+    //Stroke is a hex value
+    //Size is an integer
+    //https://kgolid.github.io/chromotome-site/
+    let { background, colors, stroke, size } = palette;
+    //Setting background to a default white if no background exists.
+    background = background ? background : "#FFF";
+
+    let startX = p.random(0, p.width / 100);
+    let startY = p.random(0, p.height / 100);
+
+    p.setup = () => {
+        defaultSetup();
+        //Do your setup here ⬇️
+        p.frameRate(frameRate);
+        canvas.background(background);
+        buffer.background(background);
+    };
+
+    p.draw = () => {
+        // Clear buffer each frame
+        defaultDraw();
+        //Draw here :) ⬇️
+        buffer.noStroke();
+
+        buffer.stroke(p.random(colors));
+        let newX = p.random(startX, startY + p.width / 5);
+        let newY = p.random(startY, startY + p.height / 10);
+        buffer.line(startX, startY, newX, newY);
+        startX = newX;
+        startY = newY;
+
+        //Stop drawing here ⬆️
+        // Draw buffer to canvas
+        p.image(buffer, 0, 0);
+    };
+
+    /*
+    ##########################################
+     Don't touch these functions unless you know what you're doing.:) ⬇️ 
+    ##########################################
+    */
+
+    const defaultSetup = () => {
+        let w = printingSize.width / exportRatio;
+        let h = printingSize.height / exportRatio;
+        buffer = p.createGraphics(w, h);
+        canvas = p.createCanvas(w, h);
+        // Adjust according to screens pixel density.
+        exportRatio /= p.pixelDensity();
+    };
+
+    const defaultDraw = () => {
+        buffer.clear();
+        // Transform (scale) all the drawings
+        buffer.scale(scaleRatio);
+    };
+
+    const exportHighResolution = () => {
+        scaleRatio = exportRatio;
+        // Re-create buffer with exportRatio and re-draw
+        // Get timestamp to name the ouput file
+        let timestamp = new Date().getTime();
+        // Save as PNG
+        p.save(canvas, p.str(`${name}-${timestamp}`), "png");
+        // Reset scaleRation back to original, re-create buffer, re-draw
+    };
+
+    p.keyReleased = () => {
+        if (p.key == "e" || p.key == "E") {
+            exportHighResolution();
+        }
+    };
+};
+
+export { name, sketch, addFolder, parameters };
--- src/algos/index.js
+++ src/algos/index.js
@@ -3,11 +3,13 @@ import * as sineWave from "./sineWave";
 import * as base from "./base"; 
 import * as particles from "./particles"; 
 import * as squares from "./squares"; 
+import * as flight from "./flight"; 
 
 export default { 
   torus, 
   sineWave,
   squares, 
+  flight, 
   base,
   particles 
 };
